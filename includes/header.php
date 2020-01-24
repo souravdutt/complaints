389b195961d4be0cfad7a54262288e04a72fcd3e
souravdutt <49240259+souravdutt@users.noreply.github.com>
2020-01-23 18:32:37 -0800

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -10,5 +10,6 @@
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="shortcut" href="assets/images/icon/title.png">
     <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>-->
 </head>
 <body>
